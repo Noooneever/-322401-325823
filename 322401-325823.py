fabb78e9e0a84e5326a478c60a27710454a5fb76
arus1709 <rakowski.arkadiusz17@gmail.com>
2024-05-12 17:43:56 +0200

    dodanie objaśnienia do utworzonych funkcji w pliku 322401-325823.py

--- 322401-325823.py
+++ 322401-325823.py
@@ -1,18 +1,19 @@
-import sys
 import numpy as np
 from math import sin, cos, tan, pi
-
 class Transformacje:
     
-    def __init__(self, model='wgs84'):
+    def __init__(self,  model = 'wgs84'):
         """
         Parametry elipsoid:
             a - duża półoś elipsoidy - promień równikowy
             b - mała półoś elipsoidy - promień południkowy
             flat - spłaszczenie
             ecc2 - mimośród^2
+        + WGS84: https://en.wikipedia.org/wiki/World_Geodetic_System#WGS84
+        + Inne powierzchnie odniesienia: https://en.wikibooks.org/wiki/PROJ.4#Spheroid
+        + Parametry planet: https://nssdc.gsfc.nasa.gov/planetary/factsheet/index.html
         """
-        if model == 'wgs84':
+        if  model == 'wgs84':
             self.a = 6378137.0
             self.b = 6356752.31424518
         elif model == 'grs80':
@@ -30,13 +31,35 @@ class Transformacje:
         
     def s2r(stopnie, minuty, sekundy):
         """
-        Funkcja przelicza wartosc podaną w stopniach na warto
+        Funkcja przelicza wartość podana w stopniach, minutach, sekundach na wartość w radianach.
+        
+        INPUT:
+            stopnie  [int] : liczba stopni
+            minuty   [int] : liczba minut
+            sekundy  [float] : liczba sekund
+        
+        OUTPUT:
+            radiany :[float] : wartość podana w radianach
         """
         kat_stopnie = stopnie + minuty/60 + sekundy/3600
         radiany = kat_stopnie * pi/180
         return(radiany)
         
     def xyz2flh(self, x, y, z):
+            """
+            Funkcja służy do transformacji współrzędnych ortokartezjańskich (prostokątnych) x, y, z 
+            na współrzędne geodezyjne f, l, h.
+            INPUT:
+                self : rodzaj elipsoidy
+                x [float] : współrzędna geocentryczna x (metry)
+                y [float] : współrzędna geocentryczna y (metry)
+                z [float] : współrzędna geocentryczna z (merty)
+                    
+            OUTPUT:
+                f [float] : szerokość geodezyjna (radiany)
+                l [float] : długość geodezyjna (radiany)
+                h [float] : wysokość elipsoidalna (metry)
+            """
             l = np.arctan2(y, x)
             p = np.sqrt(x**2 + y**2)
             f = np.arctan(z / (p * (1 - self.e2)))
@@ -49,12 +72,40 @@ class Transformacje:
                     break
                 return f, l, h
     def flh2xyz(self, f, l, h):
+            """
+            Funkcja służy do transformacji współrzędnych współrzędnych geodezyjnych f, l  
+            na współrzędne w układzie ortokartozjańskim (prostokątnym) x, y, z
+            INPUT:
+                self : rodzaj elipsoidy
+                f [float] : szerokość geodezyjna (radiany)
+                l [float] : długość geodezyjna (radiany)
+                h [float] : wysokość elipsoidalna (metry)
+                    
+            OUTPUT:
+                x [float] : współrzędna geocentryczna x (metry)
+                y [float] : współrzędna geocentryczna y (metry)
+                z [float] : współrzędna geocentryczna z (metry)
+            """
             N = self.a / np.sqrt(1 - self.e2 * np.sin(f)**2)
             x = (N + h)*np.cos(f)*np.cos(l)
             y = (N + h)*np.cos(f)*np.sin(l)
             z = ((N * (1 - self.e2)) + h) * np.sin(f)
             return x, y, z
     def XYZ2neu(dX, f, l):
+            """
+            Funkcja służy do transformacji współrzędnych ortokartezjańskich (prostokątnych) x, y, z 
+            na współrzędne geodezyjne w układzie 
+            INPUT:
+                self : rodzaj elipsoidy
+                x [float] : współrzędna geocentryczna x (metry)
+                y [float] : współrzędna geocentryczna y (metry)
+                z [float] : współrzędna geocentryczna z (merty)
+                    
+            OUTPUT:
+                f [float] : szerokość geodezyjna (radiany)
+                l [float] : długość geodezyjna (radiany)
+                h [float] : wysokość elipsoidalna (metry)
+            """
             R = np.array([[-sin(f)*cos(l), -sin(l), cos(f)*cos(l)],
                           [-sin(f)*sin(l), cos(l), cos(f)*cos(l)],
                           [cos(f), 0, sin(f)]])
@@ -64,10 +115,19 @@ class Transformacje:
             u = dx[2]
             return n, e, u 
     def fl_2000(self, f, l):
+        """
+        Funkcja służy do transformacji współrzędnych geodezyjnych f, l 
+        do Państwowy Układ Współrzędnych Geodezyjnych PL-2000 x_2000, y_2000
+        INPUT:
+            self : rodzaj elipsoidy
+            f  [float] : szerokość geodezyjna (radiany)
+            l  [float] : długość geodezyjna (radiany)
+                
+        OUTPUT:
+            x_2000 [float] : współrzędna x układu współrzędnych 2000 (metry)
+            y_2000 [float] : współrzędna y układu współrzędnych 2000 (metry)
+        """
         def s2r(stopnie, minuty, sekundy):
-            """
-            Funkcja przelicza wartosc podaną w stopniach na warto
-            """
             kat_stopnie = stopnie + minuty/60 + sekundy/3600
             radiany = kat_stopnie * pi/180
             return(radiany)
@@ -106,10 +166,20 @@ class Transformacje:
         y_2000 = y_gk * m + n * 1000000 + 500000
         return(x_2000, y_2000)
     def fl_1992(self, f, l):
+        """
+        Funkcja służy do transformacji współrzędnych geodezyjnych f, l 
+        do Państwowy Układ Współrzędnych Geodezyjnych PL-1992 x_1992, y_1992
+        INPUT:
+            self : rodzaj elipsoidy
+            f  [float] : szerokość geodezyjna (radiany)
+            l  [float] : długość geodezyjna (radiany)
+                    
+        OUTPUT:
+            x_1992 [float] : współrzędna x układu współrzędnych 1992 (metry)
+            y_1992 [float] : współrzędna y układu współrzędnych 1992 (metry)
+        """
+        
         def s2r(stopnie, minuty, sekundy):
-            """
-            Funkcja przelicza wartosc podaną w stopniach na warto
-            """
             kat_stopnie = stopnie + minuty/60 + sekundy/3600
             radiany = kat_stopnie * pi/180
             return(radiany)
@@ -147,25 +217,3 @@ class Transformacje:
         x_1992 = x_gk * m - 5300000
         y_1992 = y_gk * m + 500000
         return(x_1992, y_1992)
-
-def main():
-    if len(sys.argv) < 4:
-        print("Użycie: python skrypt.py f l model")
-        print("gdzie:")
-        print("f - {}")
-        print("l - długość geograficzna w stopniach")
-        print("model - model elipsoidy (wgs84, grs80, Krasowski)")
-        return
-
-    f = Transformacje.s2r(float(sys.argv[1]), 0, 0)
-    l = Transformacje.s2r(float(sys.argv[2]), 0, 0)
-    model = sys.argv[3]
-
-    trans = Transformacje(model)
-    x, y = trans.fl_2000(f, l)
-    print(f"Współrzędne w układzie 2000: x = {x}, y = {y}")
-    x, y = trans.fl_1992(f, l)
-    print(f"Współrzędne w układzie 1992: x = {x}, y = {y}")
-
-if __name__ == "__main__":
-    main()
